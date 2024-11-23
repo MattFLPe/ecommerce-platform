d7f249924cd532a58aeaf5ad89d73eb498d15a12
Mateus Felipe <brasil.mateus748@gmail.com>
2024-11-22 23:53:39 -0300

    Added getAllProducts, getProductById and addProduct To handle the business logic

--- src/main/java/com/dev/ecommerceAPI/service/ProductService.java
+++ src/main/java/com/dev/ecommerceAPI/service/ProductService.java
@@ -1,13 +1,26 @@
 package com.dev.ecommerceAPI.service;
 
+import com.dev.ecommerceAPI.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
 
+    public Product getProductById(String id) {
+        return productRepository.findById(id).orElse(null);
+    }
 
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
 }
